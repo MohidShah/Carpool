012b982363b18838ecd4a94f49ef1b973242e525
Mohid <mohidshah10@gmail.com>
2023-12-03 17:49:42 +0500

    Issues Resolved

--- carpool/lib/Controller/loginController.dart
+++ carpool/lib/Controller/loginController.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: file_names
 
+import 'dart:convert';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:carpool/config/Constants.dart';
 import 'package:carpool/exportlinks.dart';
+import 'package:get/get_connect/http/src/response/response.dart';
 
 class LoginController extends GetxController {
   var secureText = true;
@@ -84,15 +88,57 @@ class LoginController extends GetxController {
     var userType = "";
     print(loginemail.text);
     print(loginpassword.text);
+    loginUser(loginemail.text, loginpassword.text, "passenger");
+  }
 
-    // switch (userType) {
-    //   case 'Driver':
-    //     Get.toNamed('/DriverHome');
-    //     break;
+  Future<Map<String, dynamic>> loginUser(
+      String email, String password, String role) async {
+    print("loginuserfun");
+    final String apiUrl = 'http://localhost:3000/api/users/login';
 
-    //   case 'passengers':
-    //     Get.toNamed('/Home');
-    //     break;
-    // }
+    // Create the payload
+    Map<String, dynamic> payload = {
+      'email': loginemail.text,
+      'password': loginpassword.text,
+      'role': role,
+    };
+
+    // Encode the payload to JSON
+    String payloadJson = jsonEncode(payload);
+
+    try {
+      // Make the HTTP POST request
+      final http.Response response = await http.post(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: payloadJson,
+      );
+
+      // Check if the request was successful (status code 200)
+      if (response.statusCode == 200) {
+        // Parse the JSON response
+        Map<String, dynamic> data = jsonDecode(response.body);
+        print("data ${data}");
+        // switch (userType) {
+        //   case 'Driver':
+        //     Get.toNamed('/DriverHome');
+        //     break;
+
+        //   case 'passengers':
+        //     Get.toNamed('/Home');
+        //     break;
+        // }
+        return data;
+      } else {
+        // If the request was not successful, throw an exception
+        throw Exception('Failed to login. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      // Handle any errors that occurred during the HTTP request
+      print('Error: $error');
+      throw error;
+    }
   }
 }
